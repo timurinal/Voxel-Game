2ed66e77d25b52666e8dda1f66a60493833b0ca8
Timur Inal <timurinal09@gmail.com>
2024-06-03 12:48:53 +0100

    Shadows now use an 8192x8192px texture and now have a larger range

--- VoxelGame/Rendering/ShadowMapper.cs
+++ VoxelGame/Rendering/ShadowMapper.cs
@@ -17,9 +17,9 @@ internal class ShadowMapper
 
     public const float NearPlane = 1.0f, FarPlane = 1000f;
 
-    public const int ShadowMapWidth = 4096, ShadowMapHeight = 4096;
+    public const int ShadowMapWidth = 8192, ShadowMapHeight = 8192;
 
-    public const float OrthographicSize = 45f;
+    public const float OrthographicSize = 100f;
 
     private int _depthMapFbo, _depthMap;
 
@@ -45,6 +45,7 @@ internal class ShadowMapper
     {
         GL.BindFramebuffer(FramebufferTarget.Framebuffer, _depthMapFbo);
         GL.Viewport(0, 0, ShadowMapWidth, ShadowMapHeight);
+        //GL.CullFace(CullFaceMode.Front);
         GL.Clear(ClearBufferMask.DepthBufferBit);
     }
 
@@ -52,6 +53,7 @@ internal class ShadowMapper
     {
         GL.BindFramebuffer(FramebufferTarget.Framebuffer, 0);
         GL.Viewport(0, 0, size.X, size.Y);
+        //GL.CullFace(CullFaceMode.Back);
     }
 
     internal void UpdateMatrix(Player player, Vector3 lightDir)
